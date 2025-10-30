2a51841b1b10b89eae1525fbd4ea836eba1c8662
TheRiddler <roastingguruji541@gmail.com>
2025-10-30 21:15:29 +0000

    first web-server with Go

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main // this is the package name
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the riddler"))
+}
 
 func main() {
-	fmt.Println("theriddler")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting a httpserver at port 4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
