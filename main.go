3ae4d73d51342a08f35fa04dd4f09a684f102a98
TheRiddler <roastingguruji541@gmail.com>
2025-11-01 19:31:18 +0000

    Send some headers and In javascript everything is given

--- main.go
+++ main.go
@@ -6,13 +6,34 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from the riddler"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a specific snippet"))
 }
 
 func main() {
 
-	mux := http.NewServeMux()
+	mux := http.NewServeMux() // this is a router in go lang which handles all of the requests
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/view", snippetView)
 
 	log.Print("Starting a httpserver at port 4000")
 	err := http.ListenAndServe(":4000", mux)
