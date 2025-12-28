7e78209c79cf9ede4d89643dbd71831124cf91ab
omdubey <roastingguruji541@gmail.com>
2025-12-27 23:32:23 -0500

    Starting a server in go

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module snippetbox.riddler.com
+module github.com/omdubey00/lets-go // module path is the place where it can be downloaded place where it can be downloaded from.
 
 go 1.25.5
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the riddler creating the snippetbox")) // what this line does I have to be clear in future once understanding the fundamentals of the language
+}
 
 func main() {
-	fmt.Println("Riddler")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting the server on localhost:4000")
+	err := http.ListenAndServe(":4000", mux) // since this line has potential chances of failing or throwing an error we will have to look for the error.
+	log.Fatal(err)
 }
